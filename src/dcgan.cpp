e5bc793e81f7a05bf26e78f3083e0e8dc783aef9
zeta <zezeon@msn.com>
2019-05-09 16:59:00 +0530

    libtorch test

---
+++ src/dcgan.cpp
@@ -0,0 +1,21 @@
+#include <torch/torch.h>
+using namespace std;
+
+struct Net : torch::nn::Module {
+	Net(int64_t N, int64_t M) {
+		W = register_parameter("W", torch::randn({N, M}));
+		b = register_parameter("b", torch::randn(M));
+	}
+	torch::Tensor forward(torch::Tensor input) {
+		return torch::addmm(b, input, W);
+	}
+	torch::Tensor W, b;
+};
+
+int main() {
+	Net net(4, 5);
+	for (const auto& pair : net.named_parameters()) {
+		std::cout << pair.key() << " : " << pair.value() << std::endl;
+	}
+}
+
