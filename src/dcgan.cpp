3096bf0eb4b590e4dfd1506fac417fb7bc0cecc9
zeta <zezeon@msn.com>
2019-05-10 08:21:20 +0530

    +

--- src/dcgan.cpp
+++ src/dcgan.cpp
@@ -1,21 +1,29 @@
 #include <torch/torch.h>
 using namespace std;
 
-struct Net : torch::nn::Module {
-	Net(int64_t N, int64_t M) {
-		W = register_parameter("W", torch::randn({N, M}));
-		b = register_parameter("b", torch::randn(M));
-	}
+class Net : public torch::nn::Module {
+public:
+	Net(int64_t N, int64_t M)
+		: W{register_module("Linear", torch::nn::Linear(N, M))}
+		, b{register_parameter("bias", torch::randn(M))}
+	{ }
 	torch::Tensor forward(torch::Tensor input) {
-		return torch::addmm(b, input, W);
+		return W(input) + b;
 	}
-	torch::Tensor W, b;
+
+protected:
+	torch::nn::Linear W;
+	torch::Tensor b;
 };
 
 int main() {
 	Net net(4, 5);
 	for (const auto& pair : net.named_parameters()) {
-		std::cout << pair.key() << " : " << pair.value() << std::endl;
+		cout << pair.key() << " :\n" << pair.value() << "\n\n";
 	}
+
+	auto tensor = torch::eye(3);
+	cout << tensor << endl;
+	cout << net.forward(torch::ones({2,4})) << endl;
 }
 
